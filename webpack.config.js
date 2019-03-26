bd902937355bc3f2962f5b7243ddebf860a5056e
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-03-26 09:57:45 -0400

    Add all files

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+module.exports = {
+  entry: {
+  	bundle: './src/index.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: 'dist/'
+  },
+  module: {
+    rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+        { test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]}
+    ]
+  }
+}
